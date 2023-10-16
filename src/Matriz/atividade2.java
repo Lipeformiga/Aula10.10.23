b736ba0993b39549ef27f78e46a144b824ec67c3
Filipe Formigari <formigari2804@gmail.com>
2023-10-16 19:29:02 -0300

    a

---
+++ src/Matriz/atividade2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class atividade2 {
+	
+	public static void main(String [] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int opcao; 
+		
+		System.out.println("qual opção você quer: soma (1), subtrair(2), multiplicar(3), sair(0)");
+		opcao = sc.nextInt();
+		
+		
+	}
+
+}
